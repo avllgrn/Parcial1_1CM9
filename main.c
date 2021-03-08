0fdd3c6f451e16fdf068c747945dad55f75087e4
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-08 11:13:00 -0600

    Edad, estatura, genero

--- main.c
+++ main.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 
 int main(void){
-    //Codifica un programa que calcule el area y el perimetro de un
-    //rectangulo
-
-    float base;
-    float altura;
-    float area;
-    float perimetro;
-
-    printf("Este programa calcula el area y el perimetro de un rectangulo\n\n");
+    //Codifica un programa que pida al usuario su edad, su genero y su estatura
+    //y los muestre
 
 
-    printf("Ingresa la base: ");
-    scanf("%f",&base);
-    printf("Ingresa la altura: ");
-    scanf("%f",&altura);
+    int edad;
+    float estatura;
+    char genero;
 
-    area = base * altura;
-    perimetro = 2*base + 2*altura;
+    printf("Ingresa tu genero: ");
+    scanf("%c",&genero);
+    printf("Ingresa tu edad: ");
+    scanf("%d",&edad);
+    printf("Ingresa tu estatura: ");
+    scanf("%f",&estatura);
 
-    printf("Base = %f\n",base);
-    printf("Altura = %f\n",altura);
-    printf("Area = %f\n",area);
-    printf("Perimetro = %f\n",perimetro);
+    printf("Tu genero es: %c\n",genero);
+    printf("Tu edad es: %d\n",edad);
+    printf("Tu estatura es: %f\n",estatura);
 
     return 0;
 }
