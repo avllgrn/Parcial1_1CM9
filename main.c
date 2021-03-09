2e7507b429606761f9c5e1465a3dcbe452b7ccd5
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-09 10:47:35 -0600

    Celsius a Fahrenheit

--- main.c
+++ main.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
 int main(void){
-    //Codifica un programa que pida al usuario su edad, su genero y su estatura
-    //y los muestre
+    float celsius;
+    float fahrenheit;
 
+    printf("Ingresa los celsius ");
+    scanf("%f",&celsius);
 
-    int edad;
-    float estatura;
-    char genero;
+    fahrenheit = 9.0/5.0*celsius + 32;
 
-    printf("Ingresa tu genero: ");
-    scanf("%c",&genero);
-    printf("Ingresa tu edad: ");
-    scanf("%d",&edad);
-    printf("Ingresa tu estatura: ");
-    scanf("%f",&estatura);
-
-    printf("Tu genero es: %c\n",genero);
-    printf("Tu edad es: %d\n",edad);
-    printf("Tu estatura es: %f\n",estatura);
+    printf("%f C = ",celsius);
+    printf("%f F\n",fahrenheit);
 
     return 0;
 }
-
