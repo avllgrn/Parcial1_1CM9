87a5de1a1ad04bbe0da4bc86852c1e3d6f143229
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-09 11:00:21 -0600

    Fahrenheit a Celsius

--- main.c
+++ main.c
@@ -4,13 +4,13 @@ int main(void){
     float celsius;
     float fahrenheit;
 
-    printf("Ingresa los celsius ");
-    scanf("%f",&celsius);
+    printf("Ingresa los fahrenheit ");
+    scanf("%f",&fahrenheit);
 
-    fahrenheit = 9.0/5.0*celsius + 32;
+    celsius = (fahrenheit-32) * 5.0 / 9.0;
 
-    printf("%f C = ",celsius);
-    printf("%f F\n",fahrenheit);
+    printf("%f F = ",fahrenheit);
+    printf("%f C\n",celsius);
 
     return 0;
 }
