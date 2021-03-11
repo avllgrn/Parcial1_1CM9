38f2ade21a1eb298d9730dbdc7a12923587617bd
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-11 11:14:39 -0600

    Formula general (incompleta)

--- main.c
+++ main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
 int main(void){
-    float celsius;
-    float fahrenheit;
 
-    printf("Ingresa los fahrenheit ");
-    scanf("%f",&fahrenheit);
+    float a, b, c, x1, x2;
 
-    celsius = (fahrenheit-32) * 5.0 / 9.0;
+    printf("Dame a ");
+    scanf("%f", &a);
+    printf("Dame b ");
+    scanf("%f", &b);
+    printf("Dame c ");
+    scanf("%f", &c);
 
-    printf("%f F = ",fahrenheit);
-    printf("%f C\n",celsius);
+    x1 = (-b + sqrt(pow(b,2)-4*a*c)) / (2*a);
+    x2 = (-b - sqrt(pow(b,2)-4*a*c)) / (2*a);
+
+    printf("x1 = %f\n",x1);
+    printf("x2 = %f\n",x2);
 
     return 0;
 }
