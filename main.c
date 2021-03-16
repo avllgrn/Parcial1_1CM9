0e2b04627088f2af9cbc51c02842eb246017c4e0
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-16 11:14:36 -0600

    funciones trigonometricas de math.h

--- main.c
+++ main.c
@@ -4,14 +4,26 @@ int main(void){
 
     float alpha;
     float alphaRad;
+    float senAlpha;
+    float cosAlpha;
+    float tanAlpha;
 
     printf("Ingresa tu angulo ");
     scanf("%f",&alpha);
 
     alphaRad = alpha * M_PI / 180.0;
+    senAlpha = sin(alphaRad);
+    cosAlpha = cos(alphaRad);
+    tanAlpha = tan(alphaRad);
 
-    printf("%f Grad = ",alpha);
-    printf("%f Rad\n",alphaRad);
+    printf("alpha = %f Grad\n",alpha);
+    printf("alphaRad = %f Rad\n",alphaRad);
+    printf("sin(%f) = ",alpha);
+    printf("%f\n",senAlpha);
+    printf("cos(%f) = ",alpha);
+    printf("%f\n",cosAlpha);
+    printf("tan(%f) = ",alpha);
+    printf("%f\n",tanAlpha);
 
     return 0;
 }
