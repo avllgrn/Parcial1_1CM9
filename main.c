38fabfbb08fed1f1eb5acd63bd3dbc644cc13d53
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-16 11:12:23 -0600

    Valor de M_PI

--- main.c
+++ main.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
-
+#include <math.h>
 int main(void){
 
-    float a, b, c, x1, x2;
-
-    printf("Dame a ");
-    scanf("%f", &a);
-    printf("Dame b ");
-    scanf("%f", &b);
-    printf("Dame c ");
-    scanf("%f", &c);
-
-    x1 = (-b + sqrt(pow(b,2)-4*a*c)) / (2*a);
-    x2 = (-b - sqrt(pow(b,2)-4*a*c)) / (2*a);
-
-    printf("x1 = %f\n",x1);
-    printf("x2 = %f\n",x2);
+    printf("En C, pi = %f", M_PI);
 
     return 0;
 }
